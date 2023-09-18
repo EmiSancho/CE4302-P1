e7b8890b860378ebe20059cd02d5b070c7684db5
Cris_Azofeifa <32116409+CrisAzofeifa@users.noreply.github.com>
2023-09-18 00:45:18 -0600

    Cache divided in 4 rows

--- gui/gui_elements/cache.py
+++ gui/gui_elements/cache.py
@@ -9,9 +9,14 @@ class Cache(Figure):
 
     def draw(self):
         section_width = 300
-        section_height = 100
-        y2 = self.y1 + section_height
-        self.canvas.create_rectangle(self.x1, self.y1, self.x1 + section_width, y2, fill="white")
-        center_x = (self.x1 + self.x1 + section_width) // 2
-        center_y = (self.y1 + y2) // 2
-        self.canvas.create_text(center_x, center_y, text=self.cache_name, font=self.font)
+        section_height = 160 // 4
+
+        for i in range(4):
+            self.canvas.create_rectangle(self.x1, self.y1 + section_height*i, self.x1 + section_width, self.y1+section_height*(i+1), fill="white")
+
+        text_pos_x = self.x1 + section_width + 40
+        text_pos_y = self.y1 - 20
+        self.canvas.create_text(text_pos_x, text_pos_y, text=self.cache_name, font=self.figure_name_font)
+           
+            
+        
