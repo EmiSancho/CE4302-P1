050828392839b61296d1e7bbc57478e7ae762f96
Cris_Azofeifa <32116409+CrisAzofeifa@users.noreply.github.com>
2023-10-04 11:00:29 -0600

    data bus arrows logic

--- gui/gui_elements/address_bus.py
+++ gui/gui_elements/address_bus.py
@@ -6,7 +6,7 @@ class AddressBus(Figure):
     def __init__(self, canvas):
         super().__init__(canvas)
 
-    def draw(self):
+    def draw(self, color):
         offset_coordinates = Utils.move_arrow(constants.HORIZONTAL_ARROW_POLYGON_CORDINATES, 0, 100)
-        # Dibujar la flecha en el canvas
-        self.canvas.create_polygon(offset_coordinates, fill="black", outline="black")
+        self.canvas.create_polygon(offset_coordinates, fill=color, outline="black")
+        
